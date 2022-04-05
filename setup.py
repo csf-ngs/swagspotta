d7e632073ab2df03e0e822f249c0a80622abe4e2
Heinz Axelsson-Ekker <heinz.ekker@vbcf.ac.at>
2022-04-05 23:01:39 +0200

    Bump version: 0.0.3 → 0.0.4

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://ngs.vbcf.ac.at/repo/software/swagspotta.git',
-    version='0.0.3',
+    version='0.0.4',
     zip_safe=False,
     entry_points={
         'console_scripts': [
